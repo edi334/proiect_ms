4b5f8332af7368923244e083c24ae784bcbd7a49
Vlad Criste <vladcriste2@gmail.com>
2021-05-05 07:51:08 -0700

    added geom

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double urand()
+
+
+double urand ()
 {
     return (double) rand() / (double) ((unsigned) RAND_MAX + 1);
 }
+
 
 void FunBin (double n, double p)
 {
@@ -13,7 +16,7 @@ void FunBin (double n, double p)
     double pr = (double) pow ((1 - p), n);
     double S = pr;
 
-    double u = urand();
+    double u = urand ();
 
     while (u > S)
     {
@@ -34,10 +37,38 @@ void FunBern (double p)
         printf("Evenimentul nu s-a produs\n");
 }
 
+void FunGeom1 (double p)
+{
+    double k = 0;
+    double u;
+
+    do {
+
+        u = urand ();
+        k++;
+
+    } while (u > p);
+
+    printf ("Numărul de încercări până la primul succes (inclusiv): %ld\n", k);
+}
+
+void FunGeom2 (double p)
+{
+    double u;
+    int k;
+
+    u = urand ();
+    k = (int) (log (1 - u) / log (1 - p)) + 1;
+
+    printf ("Numărul de încercări până la primul succes (inclusiv): %ld\n", k);
+}
+
+
+
 int main ()
 {
     int optiune, nr;
-    double p;
+    double p, n;
 
     printf("Simularea variabilelor aleatoare.\n");
 
@@ -47,18 +78,45 @@ int main ()
         printf("2 - Simularea distribuției Binomiale\n");
         printf("3 - Simularea distribuției Geometrică\n");
 
+        printf ("Introduceți opțiunea: \n"); scanf ("%d", &optiune);
+
         switch (optiune)
         {
-            printf("De cate ori sa fie simulată?\n");
-            scanf("%d", &nr);
+            printf("De cate ori sa fie simulată?\n"); scanf("%d", &nr);
+
             case 1:
-                printf("Probabilitatea succesului: ");
-                scanf("%ld", &p);
+                printf("Probabilitatea succesului: "); scanf("%ld", &p);
+
                 for (int i = 0; i < nr; ++i)
                     FunBern(p);
-                printf("Media este %ld\n", p);
-                printf("Dispersia este %ld", p * (1 - p));
+                
+                printf("Media este: %ld\n", p);
+                printf("Dispersia este: %ld", p * (1 - p));
                 break;
+
+            case 2:
+                printf ("Numărul de încercări: "); scanf ("%ld", &n);
+                printf ("Probabilitatea succesului: "); scanf ("%ld", &p);
+
+                for (int i = 0; i < nr; ++i)
+                    FunBin (n, p);
+
+                printf("Media este: %ld\n", n * p);
+                printf("Dispersia este: %ld", n * p * (1 - p));
+                break;
+
+            case 3:
+                printf ("Probabilitatea succesului: "); scanf ("%ld", &p);
+
+                for (int i = 0; i < nr; ++i)
+                    FunGeom1 (p);            // am lasat geom1 deocamdata
+
+                printf("Media este: %ld\n", 1 / p);
+                printf("Dispersia este: %ld", (1 - p) / (p * p));
+                break;
+
+
+
             case 4:
                 break;
         
@@ -66,7 +124,8 @@ int main ()
                 printf("Opțiune invalidă!\n");
                 break;
         }
-    }while (optiune != 4);
+
+    } while (optiune != 4);
 
 
     return 0;
